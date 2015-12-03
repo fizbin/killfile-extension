68db3f8a56fd513b937dd44465551a7df73db07a
Daniel Martin <martin@snowplow.org>
2015-12-02 22:16:40 -0500

    jpm compatibility

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 google-chrome --pack-extension=chrome-killfile-extension --pack-extension-key=chrome-killfile-extension.pem
-(cd firefox-killfile-extension && cfx xpi)
-mv firefox-killfile-extension/killfile.xpi .
+(cd firefox-killfile-extension && jpm xpi)
+mv firefox-killfile-extension/dtm-killlfile@martin.snowplow.org*.xpi .
 rm -f chrome-killfile-extension.zip
 zip -vr chrome-killfile-extension chrome-killfile-extension
 (cd zipextra && zip -vr ../chrome-killfile-extension chrome-killfile-extension)
